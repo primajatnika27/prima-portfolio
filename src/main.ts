07fc98bbf4614507eff25a021d586d43b65319a1
prima jatnika <primajatnika271995@gmail.com>
2024-04-13 13:47:02 +0700

    Auto-generated commit

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
